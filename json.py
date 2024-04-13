47442d3da8b7122a9c7785ccfc7a6b7a01a1e11a
yasmin-rocha-4 <141040277+yasmin-rocha-4@users.noreply.github.com>
2024-04-13 20:05:20 -0300

    Add files via upload

---
+++ json.py
@@ -0,0 +1,63 @@
+import urllib.request
+import json
+import os
+import ssl
+
+def allowSelfSignedHttps(allowed):
+    # bypass the server certificate verification on client side
+    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
+        ssl._create_default_https_context = ssl._create_unverified_context
+
+allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+
+# Request data goes here
+# The example below assumes JSON formatting which may be updated
+# depending on the format your endpoint expects.
+# More information can be found here:
+# https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
+data =  {
+  "Inputs": {
+    "data": [
+      {
+        "day": 0,
+        "mnth": 0,
+        "year": 0,
+        "season": 0,
+        "holiday": 0,
+        "weekday": 0,
+        "workingday": 0,
+        "weathersit": 0,
+        "temp": 0.0,
+        "atemp": 0.0,
+        "hum": 0.0,
+        "windspeed": 0.0
+      }
+    ]
+  },
+  "GlobalParameters": 0.0
+}
+
+body = str.encode(json.dumps(data))
+
+url = 'http://50ccca0e-2804-48d7-8ec5-d0664d6a77e5.eastus2.azurecontainer.io/score'
+# Replace this with the primary/secondary key or AMLToken for the endpoint
+api_key = ''
+if not api_key:
+    raise Exception("A key should be provided to invoke the endpoint")
+
+
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+req = urllib.request.Request(url, body, headers)
+
+try:
+    response = urllib.request.urlopen(req)
+
+    result = response.read()
+    print(result)
+except urllib.error.HTTPError as error:
+    print("The request failed with status code: " + str(error.code))
+
+    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+    print(error.info())
+    print(error.read().decode("utf8", 'ignore'))
